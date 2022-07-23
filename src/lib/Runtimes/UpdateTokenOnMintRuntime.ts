f22d94b538cb737a88660a6859e1ba5afc68edee
Marco Lipparini <developer@liarco.net>
2022-07-24 00:33:07 +0200

    Adding a reaction delay to the UpdateTokenOnMintRuntime

--- src/lib/Runtimes/UpdateTokenOnMintRuntime.ts
+++ src/lib/Runtimes/UpdateTokenOnMintRuntime.ts
@@ -6,6 +6,7 @@ import RuntimeInterface from "../RuntimeInterface";
 export default class UpdateTokenOnMintRuntime implements RuntimeInterface {
   public constructor(
     private contract: ERC721Contract,
+    private reactionDelay: number,
     private fromAddress: string = ethers.utils.getAddress("0x0000000000000000000000000000000000000000"),
   ) {
   }
@@ -15,8 +16,11 @@ export default class UpdateTokenOnMintRuntime implements RuntimeInterface {
       this.contract.filters.Transfer(this.fromAddress),
       async (from: string, to: string, tokenId: BigNumber) => {
         console.log(`Token #${tokenId} was minted by ${to}...`);
-    
-        await collectionDataUpdater.updateSingleToken(tokenId);
+
+        // A delay helps avoiding out-of-sync readings during the next steps...
+        setTimeout(async () => {
+          await collectionDataUpdater.updateSingleToken(tokenId);
+        }, this.reactionDelay);
       },
     );
   }
